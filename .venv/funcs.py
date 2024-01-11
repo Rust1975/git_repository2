ae3188f5b01b0c56b44c615e63da657017279774
Rustem Fayzrakhmanov <rustem-2013@yandex.ru>
2024-01-11 23:59:24 +0500

    "Исправлены ошибки в функциях"

--- .venv/funcs.py
+++ .venv/funcs.py
@@ -1,14 +1,16 @@
-def Sum(a, b):
+def Sum():
     print("Введите целое число")
     a = int(input())
     print("Введите еще одно целое число")
     b = int(input())
-    return a + b
+    print(f'Sum = {a + b}')
 
 
-def Difference(a, b):
+def Difference():
     print("Введите целое число")
     a = int(input())
     print("Введите еще одно целое число")
     b = int(input())
-    return a - b
+    print(f'Diff = {a - b}')
+
+Difference()
