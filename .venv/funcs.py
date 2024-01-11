b9c71f31feef88961d0875595845701ee9bc5c8a
Rustem Fayzrakhmanov <rustem-2013@yandex.ru>
2024-01-11 23:46:52 +0500

    "Создана функция вычитания"

--- .venv/funcs.py
+++ .venv/funcs.py
@@ -4,3 +4,11 @@ def Sum(a, b):
     print("Введите еще одно целое число")
     b = int(input())
     return a + b
+
+
+def Difference(a, b):
+    print("Введите целое число")
+    a = int(input())
+    print("Введите еще одно целое число")
+    b = int(input())
+    return a - b
